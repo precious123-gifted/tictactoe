aa37af2f145a6bb3f12308be6a5826549894c0ea
PRECIOUS OGUNAME <ogunameprecious123@gmail.com>
2023-08-07 09:38:50 +0100

    Aug  7 09:38

--- TicTacToe.js
+++ TicTacToe.js
@@ -15,21 +15,69 @@ let Boxes = document.querySelectorAll('.box')
 
 let boxArray = Array.from(Boxes)
 boxArray.forEach((box,index)=>{box.id = index})
-let tileColor = 'dark'
 
 
- const insertTileIntoBox = (box) =>{
 
-    let tile = document.createElement('div')
-    tile.classList.add(tileColor === 'dark'? 'dark' : 'light')
 
-    box.target.style.pointerEvents = 'none';
-    box.target.append(tile)
 
+let playerOne ={
+    player:'player-one',
+    tileColor:'light',
+    timeIn:true,
+    
 }
 
-Boxes.forEach((box)=>{
-box.addEventListener('click', insertTileIntoBox)    
+let playerTwo ={
+    player: 'player-two',
+    tileColor:'dark',
+    timeIn:false,
+    
+}
+
+
+
+
+const playerMoves = [
+   
+]
+
+
+let nextPlayerToMove = playerMoves.length == 0?playerOne:_
+
+
+ const insertPlayerTileIntoBox = (box) =>{
+
+      let tile = document.createElement('div')
+    tile.classList.add(nextPlayerToMove == playerOne?playerOne.tileColor:nextPlayerToMove == playerTwo?playerTwo.tileColor:_)
+
+    box.target.style.pointerEvents = 'none';
+    
+     box.target.append(tile)   
+
+   playerMoves.push(nextPlayerToMove == playerOne?playerOne:nextPlayerToMove == playerTwo?playerTwo:_)
+   
+   
+
+
+let newNextPlayerToMove = nextPlayerToMove == playerTwo?playerOne:nextPlayerToMove == playerOne ?playerTwo:_
+nextPlayerToMove = newNextPlayerToMove
+
+let lastPlayerToMove = playerMoves[playerMoves.length-1]
+
+
+console.log(playerMoves)
+console.log('next-player:',nextPlayerToMove)
+console.log('last-player:',lastPlayerToMove)
+
+
+}
+const renderInsertPlayerTileIntoBox = Boxes.forEach((box)=>{
+     
+box.addEventListener('click',insertPlayerTileIntoBox)   
 })
 
-  
+
+
+
+
+
