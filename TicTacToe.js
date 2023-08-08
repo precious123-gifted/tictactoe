2cf3c96b6ea0c04904f267fc50fcc9f2828ab2bd
PRECIOUS OGUNAME <ogunameprecious123@gmail.com>
2023-08-08 07:45:23 +0100

    Aug 8 07:45 added function to decide the winner

--- TicTacToe.js
+++ TicTacToe.js
@@ -10,7 +10,7 @@
 
 
 
-let gameBoard = document.querySelectorAll('.box')
+let gameBoard = document.querySelector('.game-board')
 let Boxes = document.querySelectorAll('.box')
 
 let boxArray = Array.from(Boxes)
@@ -22,27 +22,26 @@ boxArray.forEach((box,index)=>{box.id = index})
 
 let playerOne ={
     player:'player-one',
-    tileColor:'light',
-    
-    
+    tileColor:'light',  
 }
 
 let playerTwo ={
     player: 'player-two',
-    tileColor:'dark',
-    
-    
+    tileColor:'dark', 
 }
-
-
 
 
 const playerMoves = [
    
 ]
 
+const winningMoves = [
+   [0,1,2],[3,4,5],[6,7,8],
+   [0,3,6],[1,4,7],[2,5,8],
+   [0,4,8],[2,4,6]
+]
 
-let nextPlayerToMove = playerMoves.length == 0?playerOne:_
+let nextPlayerToMove = playerMoves.length == 0?playerOne:$
 
 
  const insertPlayerTileIntoBox = (box) =>{
@@ -55,28 +54,51 @@ let nextPlayerToMove = playerMoves.length == 0?playerOne:_
      box.target.append(tile)   
 
    playerMoves.push(nextPlayerToMove == playerOne?playerOne:nextPlayerToMove == playerTwo?playerTwo:_)
-   
-   
 
-
-let newNextPlayerToMove = nextPlayerToMove == playerTwo?playerOne:nextPlayerToMove == playerOne ?playerTwo:_
+let newNextPlayerToMove = nextPlayerToMove == playerTwo?playerOne:nextPlayerToMove == playerOne ?playerTwo:$
 nextPlayerToMove = newNextPlayerToMove
 
 let lastPlayerToMove = playerMoves[playerMoves.length-1]
-
-
 console.log(playerMoves)
 console.log('next-player:',nextPlayerToMove)
 console.log('last-player:',lastPlayerToMove)
 
- 
+let winningMoveChecker =  playerMoves.length >= 5? checkIfAPlayerHaswon() : isNaN
+
+
+
 }
 const renderInsertPlayerTileIntoBox = Boxes.forEach((box)=>{
      
 box.addEventListener('click',insertPlayerTileIntoBox)   
 })
 
+const checkIfAPlayerHaswon = () =>{
+//check all the combo array
+//if any of the combo array contains only a particular play then that particular player wins
+ 
+winningMoves.forEach(moves => {
+  
+let winner =  moves.every((index => boxArray[index].firstChild?.classList.contains('dark')) ) ? playerTwo : moves.every((index => boxArray[index].firstChild?.classList.contains('light')) ) ? playerOne : null
 
+ if(winner){
+
+  
+gameBoard.style.pointerEvents = 'none'
+
+
+console.log('winner is:',winner)
+ 
+
+  return
+ }
+
+
+});
+
+  
+
+}
 
 
 
